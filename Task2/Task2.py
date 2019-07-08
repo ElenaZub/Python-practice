8c3c329a2a86799482952fb4be5794c5bb18c5d6
Elena Zub <e.zub@intetics.com>
2019-07-08 16:19:40 +0300

    Task3

--- Task2/Task2.py
+++ Task2/Task2.py
@@ -1,25 +1,8 @@
-def second_min_value(items, second_min):
-    min = second_min[1]
-    for x in items:
-        if(min == x[1]):
-            second_min.append(items)  
-            print(x[1])
-    return second_min
-
-
-if __name__ == '__main__':
-    print("Enter number of students")
-    students_result = []
-    for x in range(int(input())):
-        name = input()
-        score = float(input())
-        students_result.append([name, score])
-    print("Students result:")    
-    print(students_result)
-    students_result.sort(key = lambda item : item[1])
-    print(students_result)
-    second_min = second_min_value(students_result, students_result[1])
-    print(second_min)
+students = [[input(), float(input())] for _ in range(int(input()))]
+students_sorted = (sorted(list(set([mark for name, mark in students]))))
+second_lowest_grade_students = sorted((name for name, mark in students if mark == students_sorted[1]), key = lambda x:x[0])
+for item in second_lowest_grade_students:
+    print(item)
 
    
 
