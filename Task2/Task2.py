a3ebc3886dc7b05783e9f6926ae892d46f171e52
ElenaZub <zubLena@bk.ru>
2019-07-08 15:15:45 +0300

    Update Task2.py
    Last was wrong because it was without sorting of names.

--- Task2/Task2.py
+++ Task2/Task2.py
@@ -1,5 +1,5 @@
 students = [[input(), float(input())] for _ in range(int(input()))]
 students_sorted = (sorted(list(set([mark for name, mark in students]))))
-second_lowest_grade_students = [name for name, mark in students if mark == students_sorted[1]]
+second_lowest_grade_students = sorted((name for name, mark in students if mark == students_sorted[1]), key = lambda x:x[0])
 for item in second_lowest_grade_students:
     print(item)
